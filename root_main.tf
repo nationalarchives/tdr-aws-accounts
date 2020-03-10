22e13a384200f4baf68947da488d91ef78834037
Paul Schwarzenberger <paul@celidor.net>
2020-03-10 16:40:52 +0000

    ses module

--- root_main.tf
+++ root_main.tf
@@ -2,3 +2,13 @@ module "iam" {
   source            = "./tdr-terraform-modules/iam"
   aws_account_level = true
 }
+
+# route53 hosted zone must already have been set up
+module "ses-eu-west-1" {
+  source                        = "./tdr-terraform-modules/ses"
+  project                       = var.project
+  environment_full_name         = lookup(local.environment_full_name_map, local.environment)
+  providers = {
+    aws = aws.eu-west-1
+  }
+}
