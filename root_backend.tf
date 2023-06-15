88fd889152013bcdb468cecc9c681afa6b3e1302
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-15 14:24:12 +0100

    Remove bucket and lock table

--- root_backend.tf
+++ root_backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tdr-terraform-state"
     key            = "aws-account.state"
     region         = "eu-west-2"
     encrypt        = true
-    dynamodb_table = "tdr-terraform-state-lock"
   }
 }
