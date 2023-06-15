bcbc83cfe4cae367dccd88952b7b5edff0ebbd82
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-15 19:18:55 +0100

    Make the logs script split logs in half

--- .github/scripts/logs.py
+++ .github/scripts/logs.py
@@ -10,7 +10,15 @@ log_group_name = f"terraform-plan-outputs-{sys.argv[3]}"
 log_stream_name = sys.argv[2]
 
 with open(sys.argv[1]) as file:
-    log_event = [{'timestamp': timestamp, 'message': file.read()}]
+    message = file.read()
+    if len(message.encode("utf-8")) > 262144:
+        message_list = message.split("\n")
+        mid_point = len(message_list) // 2
+        first_half = "\n".join(message_list[0: mid_point])
+        second_half = "\n".join(message_list[mid_point:])
+        log_event = [{'timestamp': timestamp, 'message': first_half}, {'timestamp': timestamp, 'message': second_half}]
+    else:
+        log_event = [{'timestamp': timestamp, 'message': message}]
 
 client.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
 response = client.put_log_events(logGroupName=log_group_name,
@@ -21,6 +29,7 @@ base_url = "https://eu-west-2.console.aws.amazon.com/cloudwatch/home"
 encoded_stream_name = quote_plus(quote_plus(log_stream_name))
 fragment = f"logsV2:log-groups/log-group/{log_group_name}/log-events/{encoded_stream_name}"
 url = f"{base_url}?region=eu-west-2#{fragment}"
+
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f"log-url={url}", file=fh)
--- root_variables.tf
+++ root_variables.tf
@@ -1,6 +1,5 @@
 variable "project" {
   description = "abbreviation for the project, e.g. tdr, forms the first part of resource names"
-  default     = "tdr"
 }
 
 variable "account_number" {
