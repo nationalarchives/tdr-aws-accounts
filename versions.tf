fb43854df5d6b8fe0e031633f4a367e15a624dde
Paul Schwarzenberger <paul@celidor.net>
2024-07-23 17:19:09 +0100

    CLOP-12 remove deprecated template provider

--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.4.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
   }
   required_version = ">= 1.5.0"
 }
